9fd289d25c238561b23bbeffdfe5e05475c81ea4
javadvhd <vahedi.r46@gmail.com>
2025-03-18 00:18:57 +0330

    release

--- src/packages/excalidraw/publicPath.js
+++ src/packages/excalidraw/publicPath.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== ENV.TEST) {
   /* eslint-disable */
   /* global __webpack_public_path__:writable */
   __webpack_public_path__ =
-    window.EXCALIDRAW_ASSET_PATH ||
-    `{{process.env.VITE_PACKAGES_FILES_CDN_BASE_URL}}/${process.env.VITE_PKG_NAME}@${process.env.VITE_PKG_VERSION}/dist/`;
+    window.EXCALIDRAW_ASSET_PATH || window.EXCALIDRAW_ASSET_BASE
+      ? `${window.EXCALIDRAW_ASSET_BASE}/${process.env.VITE_PKG_NAME}@${process.env.VITE_PKG_VERSION}/dist/`
+      : `https://unpkg.com/${process.env.VITE_PKG_NAME}@${process.env.VITE_PKG_VERSION}/dist/`;
 }
