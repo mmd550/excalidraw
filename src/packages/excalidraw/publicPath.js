a3ca0096836890259433c9269716fd172a676dce
javadvhd <vahedi.r46@gmail.com>
2025-03-17 13:36:57 +0330

    fix: leave configuring public path of the project to upper libs

--- src/packages/excalidraw/publicPath.js
+++ src/packages/excalidraw/publicPath.js
@@ -1,8 +1,8 @@
-import { ENV } from "../../constants";
-if (process.env.NODE_ENV !== ENV.TEST) {
-  /* eslint-disable */
-  /* global __webpack_public_path__:writable */
-  __webpack_public_path__ =
-    window.EXCALIDRAW_ASSET_PATH ||
-    `{{process.env.VITE_PACKAGES_FILES_CDN_BASE_URL}}/${process.env.VITE_PKG_NAME}@${process.env.VITE_PKG_VERSION}/dist/`;
-}
+// import { ENV } from "../../constants";
+// if (process.env.NODE_ENV !== ENV.TEST) {
+//   /* eslint-disable */
+//   /* global __webpack_public_path__:writable */
+//   __webpack_public_path__ =
+//     window.EXCALIDRAW_ASSET_PATH ||
+//     `https://unpkg.com/${process.env.VITE_PKG_NAME}@${process.env.VITE_PKG_VERSION}/dist/`;
+// }
